3085eba7d0f37dc6ff30c2c488e137c7287962b7
p0huist <76182361+p0huist@users.noreply.github.com>
2022-02-09 16:20:30 +0200

    Update guild.js

--- data/guild.js
+++ data/guild.js
@@ -1,6 +1,6 @@
 schema = mongoose.Schema({
     guildID: String,
-    prefix: { type: String, default: "e!"},
+    prefix: { type: String, default: "p0huist"},
     ban: {
         onoff: { type: String, default: "on"},
         lmite: { type: Number, default: "3"}
