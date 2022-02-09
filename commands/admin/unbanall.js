a34b01edc41de51c671a5b2e65c0716abddbd1d1
p0huist <76182361+p0huist@users.noreply.github.com>
2022-02-09 16:25:04 +0200

    Update unbanall.js

--- commands/admin/unbanall.js
+++ commands/admin/unbanall.js
@@ -5,7 +5,7 @@ module.exports = {
   name: "unbanall",
   aliases: ["unbandall"],
   description: "You can unban all the banned users",
-  usage: ["e!unbanall"],
+  usage: ["unbanall"],
   category: ["Moderation"],
   enabled: true,            
   memberPermissions: [ "ADMINISTRATOR" ],            
@@ -18,11 +18,11 @@ module.exports = {
  
  if (message.member.hasPermission("ADMINISTRATOR")) {
                     message.guild.fetchBans().then(bans => {
-                        if (bans.size == 0) { message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`**There are no banned users**`)); throw "**No members to unban**"};
+                        if (bans.size == 0) { message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`p0huist |**There are no banned users**`)); throw "**No members to unban**"};
                         bans.forEach(ban => {
                             message.guild.members.unban(ban.user.id);
                         });
-                    }).then(() => message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`**Unbanned all users**`))).catch(e => console.log(e))
+                    }).then(() => message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`p0huist |**Unbanned all users**`))).catch(e => console.log(e))
                 } 
         }
       }
